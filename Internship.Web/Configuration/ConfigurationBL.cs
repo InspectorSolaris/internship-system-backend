0d6d19a6c36d7d273cad57a5cf832f758821d77b
German Odilov <german.odilov@gmail.com>
2021-05-30 20:45:00 +0700

    Configuration for entities services

--- Internship.Web/Configuration/ConfigurationBL.cs
+++ Internship.Web/Configuration/ConfigurationBL.cs
@@ -1,5 +1,8 @@
-﻿using Internship.BL.Interfaces.Identity;
+﻿using Internship.BL.Interfaces.Common;
+using Internship.BL.Interfaces.Identity;
+using Internship.BL.Services;
 using Internship.BL.Services.Identity;
+using Internship.Common.Dtos;
 using Internship.Common.Dtos.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +16,18 @@ namespace Internship.Web.Configuration
             serviceCollection.AddScoped<IUserService<StudentDto>, StudentService>();
             serviceCollection.AddScoped<IUserService<HITsWorkerDto>, HITsWorkerService>();
             serviceCollection.AddScoped<IUserService<CompanyDto>, CompanyService>();
+
+            serviceCollection.AddScoped<IEntityService<DocumentDto>, DocumentService>();
+            serviceCollection.AddScoped<IEntityService<InternshipAssessmentDto>, InternshipAssessmentService>();
+            serviceCollection.AddScoped<IEntityService<InterviewDto>, InterviewService>();
+            serviceCollection.AddScoped<IEntityService<PositionDto>, PositionService>();
+            serviceCollection.AddScoped<IEntityService<PriorityCompanyDto>, PriorityCompanyService>();
+            serviceCollection.AddScoped<IEntityService<PriorityStudentDto>, PriorityStudentService>();
+            serviceCollection.AddScoped<IEntityService<SpecializationDto>, SpecializationService>();
+            serviceCollection.AddScoped<IEntityService<SubjectAssessmentDto>, SubjectAssessmentService>();
+            serviceCollection.AddScoped<IEntityService<SubjectInstanceDto>, SubjectInstanceService>();
+            serviceCollection.AddScoped<IEntityService<SubjectDto>, SubjectService>();
+            serviceCollection.AddScoped<IEntityService<TechnologyDto>, TechnologyService>();
         }
     }
 }
